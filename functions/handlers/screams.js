5f16b96b91c40c88fd0568ffbb4ba2943a330eac
양우석 <woosuk288@naver.com>
2019-10-18 15:53:11 +1300

    Refactoring & Organising

---
+++ functions/handlers/screams.js
@@ -0,0 +1,36 @@
+const { db } = require("../util/admin");
+
+exports.getAllScreams = (req, res) => {
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          id: doc.id,
+          ...doc.data()
+          // body.data().body, userHandle, createdAt, commentCount, likeCount
+        });
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+};
+
+exports.postOneScream = (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.user.handle, // from middleware
+    createdAt: new Date().toISOString()
+  };
+  db.collection("screams")
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created succesfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "somthing went wrong" });
+      console.error(err);
+    });
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAEB2fP2FllmyoMrqL9iHmj8JQMT-JnVm0",
+  authDomain: "socialappclone.firebaseapp.com",
+  databaseURL: "https://socialappclone.firebaseio.com",
+  projectId: "socialappclone",
+  storageBucket: "socialappclone.appspot.com",
+  messagingSenderId: "284406354157",
+  appId: "1:284406354157:web:37a90b9ed759571e393ce4",
+  measurementId: "G-446L61KBPB"
+};
