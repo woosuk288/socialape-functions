3d9e6ea9f0d5dedd10e85ffbb8cbf4cbaa5dfccd
양우석 <woosuk288@naver.com>
2019-10-18 20:18:19 +1300

    Image Upload
    - busboy 설치.
    - path, os, fs 사용.
    - admin.storage 사용.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const app = require("express")();
 
 const FBAtuh = require("./util/fbAuth");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/user");
+const { signup, login, uploadImage } = require("./handlers/user");
 
 // Scream routes
 app.get("/screams", getAllScreams);
@@ -12,5 +12,6 @@ app.post("/scream", FBAtuh, postOneScream);
 // users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post('/user/image', FBAtuh, uploadImage);
 
 exports.api = functions.region("asia-northeast1").https.onRequest(app);
